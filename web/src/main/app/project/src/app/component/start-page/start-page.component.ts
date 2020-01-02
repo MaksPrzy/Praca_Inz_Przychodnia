fda3f531bc74985105a355b6349885cbdae46074
MaksPrzy <maksprzybylo@gmail.com>
2020-01-02 23:03:09 +0100

    FRONT POPRAWA WIDOKÓW ORAZ BACKEND(dodanie uzytkownik-rejestracja) 2.01.20

--- web/src/main/app/project/src/app/component/start-page/start-page.component.ts
+++ web/src/main/app/project/src/app/component/start-page/start-page.component.ts
@@ -1,11 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'mp-start',
     templateUrl: './start-page.component.html',
     styleUrls: ['./start-page.component.scss']
 })
-export class StartPageComponent {
+export class StartPageComponent implements OnInit{
 
+    constructor(private router: Router){};
     searchBy: string;
+
+    ngOnInit(): void {
+    }
+
+    test(){
+        console.log('test');
+        this.router.navigate(['/wyniki-wyszukiwania'], { queryParams: { searchBy: this.searchBy} });
+    }
 }
+
+
+
+
--- web/src/main/app/project/src/app/uzytkownik/uzytkownik-rejestracja-form/uzytkownik-rejestracja-form.component.ts
+++ web/src/main/app/project/src/app/uzytkownik/uzytkownik-rejestracja-form/uzytkownik-rejestracja-form.component.ts
@@ -1,7 +1,7 @@
-import {AfterViewInit, Component} from "@angular/core";
-import {NotificationService} from "@przychodnia/service/notification/notification.service";
+import {Component} from "@angular/core";
 import {UzytkownikService} from "@przychodnia/service/uzytkownik.service";
 import {PacjentNewDto} from "@przychodnia/model/backend-model";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
     selector: 'mp-uzytkownik-rejestracja-form',
@@ -10,14 +10,35 @@ import {PacjentNewDto} from "@przychodnia/model/backend-model";
 })
 export class UzytkownikRejestracjaFormComponent {
 
+    registerForm: FormGroup = new FormGroup({
+        pesel: new FormControl(),
+        imie: new FormControl(),
+        nazwisko: new FormControl(),
+        email: new FormControl(),
+        haslo: new FormControl(),
+        telefon: new FormControl(),
+        dataUrodzenia: new FormControl(),
+        adres: new FormControl()
+    })
+
     constructor(private uzytkownikService: UzytkownikService) {
     }
 
     onRegister(): void {
-        // const newUzytkownik: PacjentNewDto = {
-        //
-        // }
-        // this.uzytkownikService.register()
+        const newUzytkownik: PacjentNewDto = {
+            pesel: this.registerForm.get('pesel').value,
+            imie: this.registerForm.get('imie').value,
+            nazwisko: this.registerForm.get('nazwisko').value,
+            email: this.registerForm.get('email').value,
+            haslo: this.registerForm.get('haslo').value,
+            telefonKomorkowy: this.registerForm.get('telefon').value,
+            dataUrodzenia: this.registerForm.get('data urodzenia').value,
+            adres: (this.registerForm.get('kodPocztowy'),
+                    this.registerForm.get('miejscowosc'),
+                    this.registerForm.get('ulica'),
+                    this.registerForm.get('numerDomu')).value,
+        }
+        this.uzytkownikService.register(newUzytkownik)
     }
 
 }
